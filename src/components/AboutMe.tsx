9c1798d89bc9562f6863dc7e44444114333c07d0
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-11-10 01:42:20 +0800

    feat: added about me paragraph

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,6 +7,7 @@ import { CurrentlyLearning, techStacks } from '@/lib/data'
 import TechStackCard from './TechStackCard'
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { FaBookmark } from "react-icons/fa";
+import AngleInfo from './AngleInfo'
 
 const AboutMe: React.FC = () => {
     const tech1 = techStacks.filter((_, i) => i % 2 == 0);
@@ -28,17 +29,42 @@ const AboutMe: React.FC = () => {
                         </MagicCard>
                     </BlurFade>
                     <BlurFade delay={0.25} inView>
-                        <MagicCard className='border rounded-lg border-opacity-20 w-[min(80vw,20rem)] md:w-[33rem] min-h-[20rem] bg-black'>
-                            <div className='text-white flex font-light gap-2 m-4 items-center'>
+                        <MagicCard className='border rounded-lg border-opacity-20 w-[min(80vw,20rem)] md:w-[32.5rem] min-h-[20rem] bg-black p-4 text-white text-sm'>
+                            <div className=' flex font-light gap-2 items-center mb-4'>
                                 <IoPersonCircleOutline className='size-5' />
                                 <p className='text-gray-300 text-sm'>A little bit about myself...</p>
                             </div> 
+                            <div className='grid grid-cols-[repeat(auto-fill,_minmax(15rem,_1fr))] mb-4'>
+                                <AngleInfo>
+                                    <p>Name: </p>
+                                    <p className='text-gray-300'>Zeus D. Elderfield</p>
+                                </AngleInfo>
+                                <AngleInfo>
+                                    <p>Age: </p>
+                                    <p className='text-gray-300'>21</p>
+                                </AngleInfo>
+                                <AngleInfo>
+                                    <p>Loc: </p>
+                                    <p className='text-gray-300'>Cebu, Philippines</p>
+                                </AngleInfo>
+                                <AngleInfo>
+                                    <p>School: </p>
+                                    <p className='text-gray-300'>University of San Carlos</p>
+                                </AngleInfo>
+                            </div>
+                            <div className='px-2 font-extralight flex flex-col max-w-full mb-4'>
+                                <p>Hello, there! You can call me Zeus.</p>
+                                <p className='text-gray-300 mt-1'>I am a BS Computer Science student currently exploring the world of Web & Mobile Development, Competitive Programming, and Data Science.  I thrive on solving challenging problems and constantly expanding my skill set through side projects. Known for my adaptability, quick learning, and persistence, I enjoy tackling new challenges and finding creative solutions.</p>
+
+                                <p className='mt-4'>If you need someone who can get the job done on time, I'm your guy.</p>
+                                <p className='text-gray-300 mt-1'>Whether I’m coding an app, competing in programming contests, or diving into data, I’m driven by a desire to build, learn, and grow in everything I do. I’m always excited to take on new challenges and make a meaningful impact through innovative solutions.</p>
+                            </div>
                         </MagicCard>
                     </BlurFade>
                 </FlexRow>
                 <FlexRow className='flex-wrap justify-center gap-2'>
                     <BlurFade delay={0.25} inView>
-                        <MagicCard className='border rounded-lg border-opacity-20 w-[min(80vw,20rem)] md:w-[33rem] aspect-[3/1] h-full bg-black overflow-hidden flex items-center'>
+                        <MagicCard className='border rounded-lg border-opacity-20 w-[min(80vw,20rem)] md:w-[32.5rem] aspect-[3/1] h-full bg-black overflow-hidden flex items-center'>
                             <Marquee pauseOnHover repeat={10} className="my-2 [--duration:25s]">
                                 {tech1.map((t, i) => {
                                     return (
---
+++ src/components/AngleInfo.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+import React from 'react'
+import { FaAngleRight } from 'react-icons/fa';
+
+interface AngleInfoProps {
+    children?: React.ReactNode;
+    className?: string;
+}
+
+const AngleInfo: React.FC<AngleInfoProps> = ({children, className}) => {
+  return (
+    <div className={cn("flex gap-1 items-center font-extralight text-sm", className)}>
+        <FaAngleRight className='size-3 mx-1'/>
+        {children}
+    </div>
+  )
+}
+
+export default AngleInfo
