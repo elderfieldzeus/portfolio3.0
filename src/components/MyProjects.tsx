90dd6eab8d74524439f0ff7ffd79f857033566f0
elderfieldzeus <elderfieldzeus24@gmail.com>
2025-02-02 01:05:44 +0800

    feat: added data science project section

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BlurFade from './ui/blur-fade'
-import { projects } from '@/lib/data'
+import { swe_projects, ds_projects } from '@/lib/data'
 import ProjectCard from './ProjectCard'
 import ToggleProjects from './ToggleProjects'
 
 const MyProjects: React.FC = () => {
+  const [isSoftwareEngineer, setIsSoftwareEngineer] = useState(true)
+  const [projects, setProjects] = useState(swe_projects);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    if (!isVisible) {
+      const timer = setTimeout(() => {
+        setProjects(isSoftwareEngineer ? swe_projects : ds_projects);
+
+        setTimeout(() => {
+          setIsVisible(true)
+        }, 100)
+      }, 200) // This should match the transition duration
+      return () => clearTimeout(timer)
+    }
+  }, [isVisible, isSoftwareEngineer])
+
+  const handleSWE = () => {
+    setIsVisible(false);
+    setIsSoftwareEngineer(true);
+  }
+
+  const handleDS = () => {
+    setIsVisible(false);
+    setIsSoftwareEngineer(false);
+  }
+
   return (
     <div className='flex flex-col gap-8 items-center font-kanit relative mt-24'>
         <div className='flex flex-col items-center gap-4'>
@@ -12,10 +39,14 @@ const MyProjects: React.FC = () => {
               <p className='text-4xl'>My Projects</p>
           </BlurFade>
           <BlurFade delay={0.4} inView>
-            <ToggleProjects />
+            <ToggleProjects 
+              isSWE = {isSoftwareEngineer}
+              showSWE = {handleSWE}
+              showDS = {handleDS} 
+            />
           </BlurFade>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-x-2 gap-y-4 max-w-[50rem] w-[80vw] md:w-full mx-[10vw]'>
+        <div className={`grid grid-cols-1 md:grid-cols-2 gap-x-2 gap-y-4 max-w-[50rem] w-[80vw] md:w-full mx-[10vw] transition-opacity duration-300 ${isVisible ? "opacity-100" : "opacity-0"}`}>
           {projects.map((project, index) => {
             return(
               <ProjectCard
--- src/components/ToggleProjects.tsx
+++ src/components/ToggleProjects.tsx
@@ -1,16 +1,22 @@
 import { cn } from '@/lib/utils'
-import React, { useState } from 'react'
+import React from 'react'
 
-const ToggleProjects: React.FC = () => {
-  const [isSoftwareEngineer, setIsSoftwareEngineer] = useState(true)
+interface ToggleProjectsProps {
+  isSWE: boolean;
+  showSWE : () => void
+  showDS: () => void
+}
+
+const ToggleProjects: React.FC<ToggleProjectsProps> = ({isSWE, showSWE, showDS}) => {
+  
 
   return (
     <div className="inline-flex rounded-full p-1 bg-muted">
       <button
-        onClick={() => setIsSoftwareEngineer(true)}
+        onClick={() => showSWE()}
         className={cn(
-          "px-3 py-1 text-xs font-medium rounded-full transition-colors",
-          isSoftwareEngineer
+          "px-3 py-1 text-xs font-medium rounded-full transition-colors duration-200",
+          isSWE
             ? "bg-white text-black"
             : "text-muted-foreground hover:bg-muted-foreground/10",
         )}
@@ -18,10 +24,10 @@ const ToggleProjects: React.FC = () => {
         Software Engineering
       </button>
       <button
-        onClick={() => setIsSoftwareEngineer(false)}
+        onClick={() => showDS()}
         className={cn(
-          "px-3 py-1 text-xs font-medium rounded-full transition-colors",
-          !isSoftwareEngineer
+          "px-3 py-1 text-xs font-medium rounded-full transition-colors duration-200",
+          !isSWE
             ? "bg-white text-black"
             : "text-muted-foreground hover:bg-muted-foreground/10",
         )}
