42c8884a4b49f1fa24ca39a64f5f6b39df52e822
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-02-19 19:50:37 +0800

    fix: made particles render after window load

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Landing from '../components/Landing'
 import AboutMe from '@/components/AboutMe'
 import MyProjects from '@/components/MyProjects'
@@ -6,14 +6,30 @@ import Particles from '@/components/ui/particles'
 import Contact from '@/components/Contact'
 
 const Home: React.FC = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleLoad = () => setIsLoaded(true);
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
   return (
     <main className='w-full overflow-hidden min-h-screen bg-black text-primary relative'>
-        <Particles
+        {
+          isLoaded &&
+          <Particles
             className="absolute inset-0 h-full"
             quantity={400}
             size={0.6}
             ease={80}
-        />
+          />
+        }
         <Landing />
         <AboutMe />
         <MyProjects />
