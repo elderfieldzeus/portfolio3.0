55503a34610ae2765ba757b30148ce762eb0f323
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-10-04 13:49:22 +0800

    chore: added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const kanit = Kanit({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio | Zeus Elderfield",
+  title: "Zeus Elderfield",
   description:
     "Explore the portfolio of Zeus Elderfield — a Software Engineer based in Cebu.",
   icons: {
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     apple: "/images/logo.png",
   },
   openGraph: {
-    title: "Portfolio | Zeus Elderfield",
+    title: "Zeus Elderfield",
     description:
       "Explore the portfolio of Zeus Elderfield — a Software Engineer based in Cebu.",
     url: "https://elderfieldzeus.com",
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Portfolio | Zeus Elderfield",
+    title: "Zeus Elderfield",
     description:
       "Explore the portfolio of Zeus Elderfield — a Software Engineer based in Cebu.",
     images: ["https://elderfieldzeus.com/images/opengraph.png"],
