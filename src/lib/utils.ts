ee2ba87198c8f99312419c71c338c7dc4797c7d3
elderfieldzeus <elderfieldzeus24@gmail.com>
2025-11-30 03:58:18 +0800

    fix: re-added deleted util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,42 @@
 import { clsx, type ClassValue } from "clsx";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function sleep(ms: number) {
+export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export const handlePageTransition = async (
+  href: string,
+  router: AppRouterInstance,
+) => {
+  const body = document.querySelector("body");
+
+  body?.classList.add("page-transition");
+
+  await sleep(150);
+
+  router.push(href);
+
+  await sleep(1400);
+
+  body?.classList.remove("page-transition");
+};
+
+export const handleHrefRoute = (href: string, router: AppRouterInstance) => {
+  if (href.startsWith("/")) {
+    // handlePageTransition(href, router);
+    router.push(href);
+  } else if (href.startsWith("#")) {
+    const targetElement = document.querySelector(href);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  } else {
+    window.open(href, "_blank");
+  }
+};
